a87f68e401b4933fdcd1d347021ad36abf360bf2
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-15 16:56:05 +0300

    Rename endpoint to download teams files by page number

--- app/routers.py
+++ app/routers.py
@@ -3,10 +3,10 @@ from app.services import teams
 
 router = APIRouter()
 
-@router.post("/teams")
+@router.post("/teams-page")
 async def download_teams_files(
-    link: str = Query(..., description="link")
+    page: int = Query(..., description="page number")
 ):
-    teams.scrape_link(link)
+    teams.scrape_link(page)
     return
 
