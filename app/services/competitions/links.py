3e41522978e63a0e787719fbfcb586d51655d14e
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-15 17:07:27 +0300

    Add function to retrieve all competition links from Teknofest website

---
+++ app/services/competitions/links.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_all_links(lang="tr"):
+    link0 = f"https://teknofest.org/{lang}/yarismalar/"
+    response0 = requests.get(link0)
+    content0 = response0.content
+    soup0 = BeautifulSoup(content0, 'html.parser')
+    the_list0 = soup0.find('div', class_="tab-content mt-5 mobile-container", id="program")
+
+    items0 = the_list0.find_all('a', class_='btn')
+
+    list_of_links = []
+    for i in items0:
+        x = "https://teknofest.org" + i.get('href')
+        if x not in list_of_links:
+            list_of_links.append(x)
+
+    # with open("links.txt", 'w') as f: 
+    #     for item in list_of_links:
+    #         x = item + "\n"
+    #         f.write(x)
+
+    return list_of_links
