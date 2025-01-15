dd05146165e0c5634b248b51c0bd70b7e9bfba1a
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-15 16:59:28 +0300

    Change teams-page endpoint to GET and add teams-all endpoint for downloading multiple pages

--- app/routers.py
+++ app/routers.py
@@ -3,10 +3,17 @@ from app.services import teams
 
 router = APIRouter()
 
-@router.post("/teams-page")
+@router.get("/teams-page")
 async def download_teams_files(
     page: int = Query(..., description="page number")
 ):
-    teams.scrape_link(page)
+    teams.scrape_page(page)
     return
 
+@router.get("/teams-all")
+async def download_all_teams_files(
+    first_page: int = Query(..., description="first page number"),
+    last_page: int = Query(..., description="last page number")
+):
+    teams.scrape_all_links(first_page, last_page)
+    return
