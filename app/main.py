d83f02928ca4b3513bb5a351806d2d689c6749e6
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-18 16:09:35 +0300

    Update main.py to include all_routers for improved routing management

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from app.routers import router
+from app.routers import all_routers
 from app.initializers import dir
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(all_routers.router)
 
 # initializers
 dir.set_working_directory()
