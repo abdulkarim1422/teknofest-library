52b0750dad16c59db08f5c235978cdad3153fb0e
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-18 15:46:37 +0300

    Add endpoint for downloading announcements files with pagination support

--- app/routers.py
+++ app/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Query
 from app.services import teams
 from app.services.competitions import links_service, scrape
 from app.services.dates import call
+from app.services import announcements
 
 router = APIRouter()
 
@@ -51,4 +52,14 @@ async def get_competition_dates(
     lang: str = Query("tr", description="language")
 ):
     call.process_links(lang)
+    return
+
+@router.get("/announcements-files")
+async def download_announcements_files(
+    year: int = Query(2025, description="year"),
+    firstpage: int = Query(1, description="first page number"),
+    lastpage: int = Query(3, description="last page number"),
+    lang: str = Query("tr", description="language")
+):
+    announcements.call.main(year, firstpage, lastpage, lang)
     return
