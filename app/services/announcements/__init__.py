b1b5e81320b120ba18fdd22ff10186c6b2184165
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-18 15:46:34 +0300

    Add imports for links_service, page_service, file_service, and call in announcements module

---
+++ app/services/announcements/__init__.py
@@ -0,0 +1,4 @@
+from app.services.announcements import links_service
+from app.services.announcements import page_service
+from app.services.announcements import file_service
+from app.services.announcements import call
