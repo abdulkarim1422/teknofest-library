cecd459c7b76e9bf633a58d924925a81318b7a17
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-18 15:57:22 +0300

    Refactor file download path to use os.path.join for better compatibility

--- app/services/announcements/call.py
+++ app/services/announcements/call.py
@@ -1,6 +1,7 @@
 from app.services.announcements.links_service import api_parse_page
 from app.services.announcements.page_service import parsing_announcement_page
 from app.services.announcements.file_service import download_the_files, sanitize_filename
+import os 
 
 # list all links
 # call a function for every link
@@ -19,8 +20,9 @@ def main(year=2025, firstpage: int = 1, lastpage: int = 3, lang="tr"):
             announcement_page_url = link
 
             file_urls  = parsing_announcement_page(announcement_page_url)
-            DOWNLOAD_FOLDER = str(str(year) + '/' + date + "_" + sanitize_filename(title))
-            download_the_files(file_urls, DOWNLOAD_FOLDER)
+            # DOWNLOAD_FOLDER = str(str(year) + '/' + date + "_" + sanitize_filename(title))
+            FILE_PATH = os.path.join(os.getcwd(), "announcement_files", str(year), date + "_" + sanitize_filename(title))
+            download_the_files(file_urls, FILE_PATH)
 
         print(f"End of page: {page}, {year}")
 
