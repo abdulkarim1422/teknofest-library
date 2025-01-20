3edeebb859a1df10e04f3678efca60ca654b9461
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-20 22:06:25 +0300

    Add delay in scrape_all_links function to manage request rate

--- app/services/teams/call.py
+++ app/services/teams/call.py
@@ -1,9 +1,18 @@
 from app.services.teams import scrape 
+import time
 
-def scrape_all_links(first_page=1, last_page=200):   
+def scrape_all_links(first_page=1, last_page=200):
+    count = 0
     for i in range(first_page, last_page):
         print(f"Processing page: {i}")
         scrape.scrape_page(i)
+        
+        # Wait for 300 seconds (5 minutes) after every 10 pages
+        count += 1
+        if (count) % 10 == 0:
+            print("Waiting for 5 minutes...")
+            time.sleep(300)
+
 
 if __name__ == "__main__":
     link0 = "https://teknofest.org/tr/competitions/competition_report/"
