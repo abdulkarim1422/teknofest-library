5305c216704f2aed9927438f423fdddf0bb53eec
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-26 15:19:23 +0300

    Add tags to included routers for better API documentation

--- app/routers/all_routers.py
+++ app/routers/all_routers.py
@@ -3,10 +3,10 @@ from app.routers import competitions, dates, teams, announcements
 
 router = APIRouter()
 
-router.include_router(competitions.router)
-router.include_router(dates.router)
-router.include_router(teams.router)
-router.include_router(announcements.router)
+router.include_router(competitions.router, tags=["competitions"])
+router.include_router(dates.router, tags=["dates"])
+router.include_router(teams.router, tags=["teams"])
+router.include_router(announcements.router, tags=["announcements"])
 
 @router.get("/")
 async def root():
