018aa5fdac8e824aa470e77d28c066ad94adf654
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-01 08:39:19 +0300

    Add Alembic configuration and migration scripts for database management

---
+++ alembic/env.py
@@ -0,0 +1,56 @@
+from logging.config import fileConfig
+from sqlmodel import SQLModel
+from sqlalchemy import create_engine
+from alembic import context
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+from app import models
+from app.initializers import db
+
+target_metadata = SQLModel.metadata
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode."""
+    connectable = db.get_session().bind
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
