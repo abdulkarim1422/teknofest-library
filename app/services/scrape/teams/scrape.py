3affa225fe445142d6ddf03c35db9e07dfffd33a
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-03 03:13:45 +0300

    Add functionality to scrape team details and update database

--- app/services/scrape/teams/scrape.py
+++ app/services/scrape/teams/scrape.py
@@ -4,8 +4,9 @@ from urllib.parse import urlparse, unquote, urljoin
 from bs4 import BeautifulSoup
 from app.services import download
 from app.services.unify.function import find_original_sentence
+from app.services.repo_additional import team_crud_services
 
-def scrape_page(page):
+def scrape_page(page, update_database: bool = False):
     try:
         link = f"https://teknofest.org/tr/competitions/competition_report/?search=&page={page}"
         response0 = requests.get(link)
@@ -40,6 +41,22 @@ def scrape_page(page):
                         download.download_file(team_link, full_intro_file_path)
                     except:
                         print(f"team file failed for {team_name}")
+
+                    if update_database:
+                        team_name, team_members_list, team_info, institution_name = scrape_team_page(team_link)
+                        team_crud_services.update_or_create_team(
+                            name=team_name,
+                            members_list=team_members_list,
+                            description=team_info,
+                            institution_name=institution_name,
+                            comp_name=comp_name,
+                            year=year,
+                            report_file_path=full_report_file_path,
+                            intro_file_path=full_intro_file_path,
+                            team_link=team_link,
+                            status="finalist"
+                            )
+                        pass
                     
                 except (AttributeError, IndexError, KeyError) as e:
                     print(f"Skipping a row due to missing data: {e}")
@@ -48,3 +65,36 @@ def scrape_page(page):
             print("The specified element was not found.")
     except requests.exceptions.RequestException as e:
         print(f"Error while fetching the page: {e}")
+
+def scrape_team_page(team_link):
+    try:
+        team_page = requests.get(team_link)
+        team_page.raise_for_status()
+        team_page_soup = BeautifulSoup(team_page.text, 'html.parser')
+
+        # team name
+        team_name = team_page_soup.find('h1').text.strip()
+
+        # team members list
+        team_members_list = []
+        members_row = team_page_soup.find('div', class_='report-team')
+        for member_element in members_row.find_all('p'):
+            member_name = member_element.text.strip()
+            team_members_list.append(member_name)
+
+        # team info
+        team_info_div = team_page_soup.find('div', class_='team-info')
+        team_info_h6 = team_info_div.find('h6')
+        if team_info_h6:
+            team_info_h6.extract()
+        team_info = team_info_div.text.strip()
+
+        institution_name = team_page_soup.find('p', class_='school').text.strip()
+
+        return team_name, team_members_list, team_info, institution_name
+    except requests.exceptions.RequestException as e:
+        print(f"Error while fetching the page: {e}")
+        return None, None, None
+    
+
+
