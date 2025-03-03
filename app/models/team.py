e4c04c86233261ea9d0ab73f32c959f22ea317bc
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-03 03:13:22 +0300

    Refactor Team model to use Optional fields and add new attributes for institution name, tap members, and file paths

--- app/models/team.py
+++ app/models/team.py
@@ -10,12 +10,16 @@ class Team(SQLModel, table=True):
     updated_at: datetime = Field(default=datetime.utcnow)
     deleted_at: Optional[datetime] = None
     description: str
-    stage: str # University, High School, Middle School, etc.
-    member_count: int
-    members: list[uuid.UUID]
-    leader: uuid.UUID
+    stage: Optional[str]: None # University, High School, Middle School, etc.
+    institution_name: Optional[str] = None
+    member_count: Optional[int] = None
+    tap_members: Optional[list[uuid.UUID]] = None
+    members_list: Optional[list[str]] = None
+    leader: Optional[uuid.UUID] = None
     competition_id: uuid.UUID
     years: list[int]
-    status: str
-    rank: int
-    relation: str # central, related, friend, none
+    status: Optional[str] = None # "finalist", "derece", "etc"
+    rank: Optional[int] = None
+    relation: Optional[str] = None # central, related, friend, None
+    intro_file_path: Optional[str] = None
+    team_link: Optional[str] = None
