626c7c53661a5665b74832f7d9bf53543a0a8b40
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-03 22:19:07 +0300

    Refactor update_or_create_team to streamline team creation and updating logic, ensuring proper handling of team attributes and report file creation.

--- app/services/repo_additional/team_crud_services.py
+++ app/services/repo_additional/team_crud_services.py
@@ -9,7 +9,8 @@ def get_teams_by_competition_en_name(name: str):
     competition_obj = competition_crud_class.get_competition_by_en_name(name)
     if competition_obj is None:
         return None
-    return team_crud.TeamCRUD.get_teams_by_competition_id(competition_obj.id)
+    team_crud_class = team_crud.TeamCRUD()
+    return team_crud_class.get_teams_by_competition_id(competition_obj.id)
 
 def update_or_create_team(
     name,
@@ -40,10 +41,33 @@ def update_or_create_team(
         competition_obj = competition_crud_services.get_competition_obj_via_any_name(comp_name)
         if competition_obj:
             competition_id = competition_obj.id
+
+        team_obj_new: Team = Team()
         team_crud_class = team_crud.TeamCRUD()
-        db_team = team_crud_class.get_team_by_name_and_year(name=name, year=int(year))
-        if db_team is None: # create new team
-            db_team = Team(
+        team_obj_from_db = team_crud_class.get_team_by_name_and_year(name=name, year=int(year))
+
+        if team_obj_from_db: # update existing team
+            if members_list:
+                team_obj_new.members_list = members_list
+            if description:
+                team_obj_new.description = description
+            if institution_name:
+                team_obj_new.institution_name = institution_name
+            if status:
+                team_obj_new.competition_id = competition_id
+            if year:
+                team_obj_new.year = year
+            if intro_file_path:
+                team_obj_new.intro_file_path = intro_file_path
+            if team_link:
+                team_obj_new.team_link = team_link
+            if status:
+                team_obj_new.status = status
+
+            team_crud_class.update_team(team_obj_from_db.id, team_obj_new)
+            team_obj_new.id = team_obj_from_db.id # to use in report_file creation
+        else: # create new team
+            team_obj_new = Team(
                 name=name,
                 members_list=members_list,
                 description=description,
@@ -54,32 +78,20 @@ def update_or_create_team(
                 team_link=team_link,
                 status=status
             )
-            team_crud_class.create_team(db_team)
+            team_crud_class.create_team(team_obj_new)
 
-        else: # update existing team
-            db_team.members_list = members_list
-            db_team.description = description
-            db_team.institution_name = institution_name
-            db_team.competition_id = competition_id
-            db_team.year = year
-            db_team.intro_file_path = intro_file_path
-            db_team.team_link = team_link
-            db_team.status = status
-
-            team_crud.TeamCRUD.update_team(db_team.id, db_team)
-
-        
         if report_file_path is not None:
             report_file = Report_File(
                 competition_id=competition_id,
-                team_id=db_team.id,
+                team_id=team_obj_new.id,
                 year=year,
                 file_path=report_file_path,
                 rank=status
             )
-            team_crud.ReportFileCRUD.create_report_file(report_file)
+            report_file_crud_class = team_crud.ReportFileCRUD()
+            report_file_crud_class.create_report_file(report_file)
 
-        return db_team
+        return team_obj_new
     except Exception as e:
         print(f"Error while updating or creating team: {e}")
         return None
