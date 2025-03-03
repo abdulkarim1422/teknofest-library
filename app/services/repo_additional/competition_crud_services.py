b2fff71d90cc0fbdf2132119fb6a81d8efcaf198
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-03 17:28:12 +0300

    Refactor update_or_create_competition function to streamline competition creation and update logic

--- app/services/repo_additional/competition_crud_services.py
+++ app/services/repo_additional/competition_crud_services.py
@@ -53,66 +53,56 @@ def update_or_create_competition(
             lang: {lang}
         """)
     
-    if "teknofest.org/tr" in link:
+    competition_obj_new: Competition = Competition()
+
+    competition_obj_from_db = get_competition_en_name_via_any_name(comp_name)
+    competition_crud_class = competition_crud.CompetitionCRUD()
+    if competition_obj_from_db:
+        competition_id = competition_obj_from_db.id
+        competition_obj_new = competition_crud_class.get_competition(competition_id)
+
+
+    if lang == "tr" or "teknofest.org/tr" in link:
         lang = "tr"
-        competition_obj = get_competition_en_name_via_any_name(tr_name)
-    elif "teknofest.org/en" in link:
+        competition_obj_new.application_link_tr = application_link
+        competition_obj_new.tr_name = comp_name
+        competition_obj_new.tr_description = comp_description
+        competition_obj_new.tr_link = comp_link
+    elif lang == "en" or "teknofest.org/en" in link:
         lang = "en"
-        competition_obj = get_competition_en_name_via_any_name(en_name)
+        competition_obj_new.application_link_en = application_link
+        competition_obj_new.en_name = comp_name
+        competition_obj_new.en_description = comp_description
+        competition_obj_new.en_link = comp_link
     else:
         lang = "ar"
-        competition_obj = get_competition_en_name_via_any_name(ar_name)
+        competition_obj_new.application_link_ar = application_link
+        competition_obj_new.ar_name = comp_name
+        competition_obj_new.ar_description = comp_description
+        competition_obj_new.ar_link = comp_link
 
-    if competition_obj:
-        competition_id = competition_obj.id
-    competition_crud_class = competition_crud.CompetitionCRUD()
-    db_competition = competition_crud_class.get_competition(competition_id)
 
-    if db_competition is None:  # create new competition
-        db_competition = Competition(
-            image_path=image_link,
-            tk_number=tk_number,
-            t3kys_number=t3kys_number,
-            application_link_tr=application_link_tr,
-            application_link_en=application_link_en,
-            application_link_ar=application_link_ar,
-            tr_name=tr_name,
-            tr_description=tr_description,
-            tr_link=tr_link,
-            en_name=en_name,
-            en_description=en_description,
-            en_link=en_link,
-            ar_name=ar_name,
-            ar_description=ar_description,
-            ar_link=ar_link,
-            years=[year],
-            min_member=min_member,
-            max_member=max_member
-        )
-        competition_crud_class.create_competition(db_competition)
+
+    if competition_obj_from_db is None:  # create new competition
+        competition_obj_new.image_path=image_link
+        competition_obj_new.tk_number=tk_number
+        competition_obj_new.t3kys_number=t3kys_number
+        competition_obj_new.years=[year]
+        competition_obj_new.min_member=min_member
+        competition_obj_new.max_member=max_member
+
+        competition_crud_class.create_competition(competition_obj_new)
 
     else:  # update existing competition
-        db_competition.image_path = image_link
-        db_competition.tk_number = tk_number
-        db_competition.t3kys_number = t3kys_number
-        db_competition.application_link_tr = application_link_tr
-        db_competition.application_link_en = application_link_en
-        db_competition.application_link_ar = application_link_ar
-        db_competition.tr_name = tr_name
-        db_competition.tr_description = tr_description
-        db_competition.tr_link = tr_link
-        db_competition.en_name = en_name
-        db_competition.en_description = en_description
-        db_competition.en_link = en_link
-        db_competition.ar_name = ar_name
-        db_competition.ar_description = ar_description
-        db_competition.ar_link = ar_link
-        db_competition.years.append(year)
-        db_competition.min_member = min_member
-        db_competition.max_member = max_member
+        competition_obj_new.image_path = image_link
+        competition_obj_new.tk_number = tk_number
+        competition_obj_new.t3kys_number = t3kys_number
+        competition_obj_new.years.append(year)
+        competition_obj_new.min_member = min_member
+        competition_obj_new.max_member = max_member
 
-        competition_crud_class.update_competition(competition_id, db_competition)
+        competition_crud_class.update_competition(competition_id, competition_obj_new)
 
-    return db_competition
+    return competition_obj_new
 
 
