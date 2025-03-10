4168d8a0a230933299de1971e25c6981347c6539
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-10 23:04:31 +0300

    Add options to update downloads and database in team file scraping endpoints

--- app/routers/teams.py
+++ app/routers/teams.py
@@ -5,16 +5,19 @@ router = APIRouter()
 
 @router.get("/teams-page")
 async def download_teams_files(
-    page: int = Query(..., description="page number")
+    page: int = Query(..., description="page number"),
+    update_downloads: bool = Query(False, description="update downloads"),
+    update_database: bool = Query(False, description="update database")
 ):
-    scrape.scrape_page(page)
+    scrape.scrape_page(page=page, update_downloads=update_downloads, update_database=update_database)
     return
 
 @router.get("/teams-all")
 async def download_all_teams_files(
     first_page: int = Query(..., description="first page number"),
     last_page: int = Query(..., description="last page number"),
+    update_downloads: bool = Query(False, description="update downloads"),
     update_database: bool = Query(False, description="update database")
 ):
-    call.scrape_all_links(first_page, last_page, update_database)
+    call.scrape_all_links(first_page=first_page, last_page=last_page, update_downloads=update_downloads, update_database=update_database)
     return
