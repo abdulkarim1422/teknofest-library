e950fe024213c006185427971b6a405698c2c01b
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-13 16:03:59 +0300

    Add repository router to include repo endpoints

--- app/routers/all_routers.py
+++ app/routers/all_routers.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Query
 from app.routers import scrape_routers
+from app.routers import repo_routers
 
 router = APIRouter()
 
 router.include_router(scrape_routers.router, prefix="/api/scrape")
+router.include_router(repo_routers.router, prefix="/api/repo")
 
 @router.get("/")
 async def root():
